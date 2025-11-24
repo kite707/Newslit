b8e8148efac2d5f39d979f3776533d1ea345e513
kite707 <kite707707@gmail.com>
2025-11-24 22:56:08 +0900

    refactor: 일일 컨텐츠 분할 메서드 PostMapping으로 변경

--- backend/src/main/java/com/newslit/backend/daily/DailyController.java
+++ backend/src/main/java/com/newslit/backend/daily/DailyController.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DailyController {
     private final DailyService dailyService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<List<DailyResponseDto>> split(@RequestParam Long id) {
         List<DailyResponseDto> chunks = dailyService.createChunks(id);
         return ResponseEntity.ok(chunks);
     }
+
 }
