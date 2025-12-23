4aeb185d0fbe0d5052d46b41bab645ed2d96274e
kite707 <kite707707@gmail.com>
2025-12-23 16:02:15 +0900

    fix: AuthResponseDto 어노테이션 추가

--- backend/src/main/java/com/newslit/backend/user/dto/AuthResponseDto.java
+++ backend/src/main/java/com/newslit/backend/user/dto/AuthResponseDto.java
@@ -1,7 +1,13 @@
 package com.newslit.backend.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthResponseDto {
     private String message;
