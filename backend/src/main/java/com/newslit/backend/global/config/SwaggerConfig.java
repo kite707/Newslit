0c039b5c69b434ec6a6d1e0b67f17c0ba6b4dd47
kite707 <kite707707@gmail.com>
2025-12-27 13:36:54 +0900

    feat: 스웨거 jwt 방식 설정

--- backend/src/main/java/com/newslit/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/newslit/backend/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.newslit.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
         return new OpenAPI()
                 .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("BearerAuth", securityScheme)
                 .info(apiInfo());
     }
 
