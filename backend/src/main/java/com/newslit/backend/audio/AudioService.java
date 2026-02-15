24f8a8b509c68ef7cf9f4b3d1866ec878cb3081b
kite707 <kite707707@gmail.com>
2026-02-15 22:28:35 +0900

    fix : 문장 일치 로직 개선 - 단어 단위로 stamp 가져오도록 변경 - 편집 함수 계산 로직 구현 - 단어별로 비교해 단어가 일치하거나 유사할 경우 pass 해가며 endTime 구하도록 로직 구현

--- backend/src/main/java/com/newslit/backend/audio/AudioService.java
+++ backend/src/main/java/com/newslit/backend/audio/AudioService.java
@@ -4,15 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.newslit.backend.article.Article;
 import com.newslit.backend.article.ArticleRepository;
 import com.newslit.backend.article.exception.ArticleNotFoundException;
-import com.newslit.backend.audio.dto.SegmentDto;
 import com.newslit.backend.audio.dto.WhisperResponseDto;
 import com.newslit.backend.audio.dto.WordDto;
 import com.newslit.backend.sentence.Sentence;
+import com.oracle.bmc.objectstorage.ObjectStorage;
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
 import jakarta.transaction.Transactional;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,10 +39,8 @@ public class AudioService {
 
     private static final String WHISPER_MODEL = "whisper-1";
     private static final String RESPONSE_FORMAT = "verbose_json";
-    private static final String TIMESTAMP_GRANULARITY = "segment";
+    private static final String TIMESTAMP_GRANULARITY = "word";
     private static final MediaType AUDIO_MEDIA_TYPE = MediaType.parse("audio/mpeg");
-    private static final int LONG_SENTENCE_MATCH_LENGTH = 10;
-    private static final double SHORT_SENTENCE_MATCH_RATIO = 0.8;
 
     @Value("${openai.api-key}")
     private String apiKey;
@@ -54,7 +56,7 @@ public class AudioService {
 
     private final OkHttpClient httpClient;
     private final ObjectMapper objectMapper;
-    //    private final ObjectStorage objectStorage;
+    private final ObjectStorage objectStorage;
     private final ArticleRepository articleRepository;
 
     @Transactional
@@ -65,8 +67,7 @@ public class AudioService {
 
         File audioFile = downloadAudioFile(article.getAudioDownloadLink());
 
-        //TODO : 추후 주석해제
-//        uploadMp3ToStorage(articleId, audioFile);
+        uploadMp3ToStorage(articleId, audioFile);
 
         try {
             List<WordDto> words = transcribe(audioFile, article);
@@ -105,45 +106,64 @@ public class AudioService {
     private void matchAndSaveTimestamps(List<WordDto> words, List<Sentence> sentences) {
         int wordsStart = 0;
         for (Sentence sentence : sentences) {
-            String englishText = normalizeText(sentence.getEnglishText());
-            System.out.println("[CurSentence]" + englishText.replaceAll("\\s+", ""));
+            int endIdx = getEndIdx(sentence, words, wordsStart);
 
-            boolean matched = false;
-            StringBuilder accumulated = new StringBuilder();
-            double startTime = -1;
+            sentence.setStartTime(words.get(wordsStart).getStart());
+            sentence.setEndTime(words.get(endIdx).getEnd());
 
-            for (int wordsIdx = wordsStart; wordsIdx < words.size(); wordsIdx++) {
-                WordDto word = words.get(wordsIdx);
-                String normalizedWord = normalizeText(word.getWord());
+            wordsStart = endIdx + 1;
+        }
+    }
 
-                accumulated.append(normalizedWord);
-
-                if (startTime == -1) {
-                    startTime = word.getStart();
-                }
-
-                if (englishText.replaceAll("\\s+", "").equals(accumulated.toString().replaceAll("\\s+", ""))) {
-                    System.out.println("[SegmentText] " + accumulated.toString().replaceAll("\\s+", ""));
-                    sentence.setStartTime(startTime);
-                    sentence.setEndTime(word.getEnd());
-                    matched = true;
-                    System.out.println("[SUCCESS] Matched!");
-                    wordsStart = wordsIdx + 1;
+    private int getEndIdx(Sentence sentence, List<WordDto> words, int start) {
+        int startIdx = start;
+        List<String> sentenceWords = List.of(sentence.getEnglishText().split(" "));
+        for (String word : sentenceWords) {
+            for (int i = startIdx; i < words.size(); i++) {
+                String curWord = words.get(i).getWord();
+                if (isSimilar(word, curWord)) {
+                    startIdx++;
                     break;
                 }
             }
-
-            if (!matched) {
-                System.out.println("[FAILED] " + accumulated.toString().replaceAll("\\s+", ""));
-            }
         }
+        return startIdx - 1;
     }
 
     private String normalizeText(String text) {
         return text.replaceAll("[^\\w\\s]", "")  // 특수문자 제거 (공백은 유지)
-                .replaceAll("\\s+", " ")       // 연속 공백을 하나로
+                .replaceAll("\\s+", "")       // 연속 공백을 하나로
                 .toLowerCase()
                 .trim();
+    }
+
+    private boolean isSimilar(String word1, String word2) {
+        word1 = normalizeText(word1);
+        word2 = normalizeText(word2);
+
+        if (word1.equals(word2)) {
+            return true;
+        }
+
+        int distance = levenshteinDistance(word1, word2);
+        int maxLen = Math.max(word1.length(), word2.length());
+        return distance <= (maxLen * 0.1);
+
+    }
+
+    public int levenshteinDistance(String word1, String word2) {
+        int[][] dp = new int[100][100];
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
     }
 
     public List<WordDto> transcribe(File audioFile, Article article) throws IOException {
@@ -153,7 +173,7 @@ public class AudioService {
                         RequestBody.create(audioFile, AUDIO_MEDIA_TYPE))
                 .addFormDataPart("model", WHISPER_MODEL)
                 .addFormDataPart("response_format", RESPONSE_FORMAT)
-                .addFormDataPart("timestamp_granularities[]", "word")
+                .addFormDataPart("timestamp_granularities[]", TIMESTAMP_GRANULARITY)
                 .addFormDataPart("prompt", article.getOriginalText())
                 .build();
 
@@ -174,83 +194,47 @@ public class AudioService {
             );
 
             return responseDto.getWords();
-
-//            return mergeSegments(responseDto);
         }
     }
 
-    private List<SegmentDto> mergeSegments(WhisperResponseDto response) {
-        List<SegmentDto> mergedSegments = new ArrayList<>();
-        SegmentDto current = null;
 
-        for (WordDto segment : response.getWords()) {
-            String text = segment.getWord().trim();
+    public void uploadMp3ToStorage(Long articleId, File audioFile) throws IOException {
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(ArticleNotFoundException::new);
 
-            if (current == null) {
-                current = new SegmentDto();
-                current.setId(mergedSegments.size());
-                current.setStart(segment.getStart());
-                current.setEnd(segment.getEnd());
-                current.setText(text);
-            } else {
-                current.setText(current.getText() + " " + text);
-                current.setEnd(segment.getEnd());
-            }
-
-            // 문장 끝 판단 (. ! ? 로 끝나면)
-            if (text.endsWith(".") || text.endsWith("!") || text.endsWith("?")) {
-                mergedSegments.add(current);
-                current = null;
-            }
+        if (!audioFile.exists()) {
+            throw new IOException("Audio file does not exist: " + audioFile.getPath());
         }
 
-        // 마지막 미완성 문장 처리
-        if (current != null) {
-            mergedSegments.add(current);
+        String objectName = article.getTitle()
+                .replaceAll("[^a-zA-Z0-9가-힣.-]", "_") + ".mp3";
+
+        // URL 인코딩 처리
+        String encodedObjectName = URLEncoder.encode(objectName, StandardCharsets.UTF_8)
+                .replace("+", "%20");
+
+        try (InputStream inputStream = new FileInputStream(audioFile)) {
+            PutObjectRequest putRequest = PutObjectRequest.builder()
+                    .namespaceName(namespace)
+                    .bucketName(bucket)
+                    .objectName(objectName)
+                    .contentLength(audioFile.length())
+                    .contentType("audio/mpeg")
+                    .putObjectBody(inputStream)
+                    .build();
+
+            objectStorage.putObject(putRequest);
+
+            String audioUrl = String.format(
+                    "https://objectstorage.%s.oraclecloud.com/n/%s/b/%s/o/%s",
+                    region,
+                    namespace,
+                    bucket,
+                    encodedObjectName
+            );
+
+            article.setAudioLink(audioUrl);
+            articleRepository.save(article);
         }
-        System.out.println("=============mergedSegments=====================");
-        mergedSegments.forEach(System.out::println);
-        System.out.println("=============mergedSegments=====================");
-        return mergedSegments;
     }
-
-//    public void uploadMp3ToStorage(Long articleId, File audioFile) throws IOException {
-//        Article article = articleRepository.findById(articleId)
-//                .orElseThrow(ArticleNotFoundException::new);
-//
-//        if (!audioFile.exists()) {
-//            throw new IOException("Audio file does not exist: " + audioFile.getPath());
-//        }
-//
-//        String objectName = article.getTitle()
-//                .replaceAll("[^a-zA-Z0-9가-힣.-]", "_") + ".mp3";
-//
-//        // URL 인코딩 처리
-//        String encodedObjectName = URLEncoder.encode(objectName, StandardCharsets.UTF_8)
-//                .replace("+", "%20");
-//
-//        try (InputStream inputStream = new FileInputStream(audioFile)) {
-//            PutObjectRequest putRequest = PutObjectRequest.builder()
-//                    .namespaceName(namespace)
-//                    .bucketName(bucket)
-//                    .objectName(objectName)
-//                    .contentLength(audioFile.length())
-//                    .contentType("audio/mpeg")
-//                    .putObjectBody(inputStream)
-//                    .build();
-//
-//            objectStorage.putObject(putRequest);
-//
-//            String audioUrl = String.format(
-//                    "https://objectstorage.%s.oraclecloud.com/n/%s/b/%s/o/%s",
-//                    region,
-//                    namespace,
-//                    bucket,
-//                    encodedObjectName
-//            );
-//
-//            article.setAudioLink(audioUrl);
-//            articleRepository.save(article);
-//        }
-//    }
 }
