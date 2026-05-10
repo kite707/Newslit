31c2164c2aba3d8e45a49a3c15bbc0c6d5ff65b5
kite707 <kite707707@gmail.com>
2026-05-10 11:23:18 +0900

    refactor: mail sender 프로퍼티로 분리

--- backend/src/main/java/com/newslit/backend/mail/MailService.java
+++ backend/src/main/java/com/newslit/backend/mail/MailService.java
@@ -2,6 +2,7 @@ package com.newslit.backend.mail;
 
 import com.newslit.backend.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Service;
 public class MailService {
     private final JavaMailSender mailSender;
 
+    @Value("${mail.verify-sender}")
+    private String sender;
+
     public void sendTestMail(String to) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@newslit.net");
+        message.setFrom(sender);
         message.setTo(to);
         message.setSubject("Newslit 테스트 메일");
         message.setText("OCI Email Delivery로 발송된 메일입니다.");
