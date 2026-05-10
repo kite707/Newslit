cb294acb568baf6b7a7e19580324939e5a7e4015
kite707 <kite707707@gmail.com>
2026-05-10 11:18:59 +0900

    refactor: 사용하지 않는 의존성 제거

--- backend/src/main/java/com/newslit/backend/mail/MailService.java
+++ backend/src/main/java/com/newslit/backend/mail/MailService.java
@@ -1,7 +1,6 @@
 package com.newslit.backend.mail;
 
 import com.newslit.backend.user.User;
-import com.newslit.backend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,8 +10,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MailService {
     private final JavaMailSender mailSender;
-    private final EmailVerificationRepository emailVerificationRepository;
-    private final UserRepository userRepository;
 
     public void sendTestMail(String to) {
         SimpleMailMessage message = new SimpleMailMessage();
