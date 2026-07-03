8f68af90e540fcb3a2b9ecb9d470367c67025a08
kite707 <kite707707@gmail.com>
2026-07-03 12:50:59 +0900

    feat: Pending 상태 RSS 조회 메서드 구현

--- backend/src/main/java/com/newslit/backend/rss/RssRepository.java
+++ backend/src/main/java/com/newslit/backend/rss/RssRepository.java
@@ -1,5 +1,6 @@
 package com.newslit.backend.rss;
 
+import com.newslit.backend.global.common.enums.Status;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ public interface RssRepository extends JpaRepository<Rss, Long> {
     Optional<Rss> findById(Long id);
 
     boolean existsByUrl(String url);
+
+    Optional<Rss> findFirstByStatusOrderByIdAsc(Status status);
 }
